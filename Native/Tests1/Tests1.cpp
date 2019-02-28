26da6bab09d13224c5999ecf32e50220b828afaa
Oscar Calvo <ocalvo@microsoft.com>
2019-02-27 17:53:30 -0800

    Add working app

--- Native/Tests1/Tests1.cpp
+++ Native/Tests1/Tests1.cpp
@@ -3,13 +3,14 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace Tests1
+namespace Tests2
 {
-    TEST_CLASS(UnitTest1)
+    TEST_CLASS(UnitTest2)
     {
     public:
         TEST_METHOD(TestMethod1)
         {
+            // TODO: Your test code here
         }
     };
 }
